41c6f63d7732cc27b73e0424c8d3f2eca8a5b32b
lauLanez <laulanyumbayire@gmail.com>
2024-03-14 09:38:04 -0500

    first commit

---
+++ Admin Dashboard Panel/Login&signup/login.js
@@ -0,0 +1,10 @@
+const signupBtn = document.getElementById("signup-btn");
+const signinBtn = document.getElementById("signin-btn");
+const mainContainer = document.querySelector(".container");
+
+signupBtn.addEventListener("click", () => {
+  mainContainer.classList.toggle("change");
+});
+signinBtn.addEventListener("click", () => {
+  mainContainer.classList.toggle("change");
+});
---
+++ Admin Dashboard Panel/script.js
@@ -0,0 +1,39 @@
+const body = document.querySelector("body"),
+      modeToggle = body.querySelector(".mode-toggle");
+      sidebar = body.querySelector("nav");
+      sidebarToggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if(getMode && getMode ==="dark"){
+    body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if(getStatus && getStatus ==="close"){
+    sidebar.classList.toggle("close");
+}
+
+modeToggle.addEventListener("click", () =>{
+    body.classList.toggle("dark");
+    if(body.classList.contains("dark")){
+        localStorage.setItem("mode", "dark");
+    }else{
+        localStorage.setItem("mode", "light");
+    }
+});
+
+sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    if(sidebar.classList.contains("close")){
+        localStorage.setItem("status", "close");
+    }else{
+        localStorage.setItem("status", "open");
+    }
+})
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var formData = new FormData(this);
+    // Here you can use AJAX to send form data to the server and handle the response
+    // For this example, let's just display the success message
+    document.getElementById("successMessage").style.display = "block";
+});
