15d061dfd53758a702bb9c3ec655d9dd77e81820
lauLanez <laulanyumbayire@gmail.com>
2024-04-02 09:40:20 -0500

    added a testimonial's carousel

--- Frontpage/new.js
+++ Frontpage/new.js
@@ -1,13 +1,57 @@
+const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
-let isDragging = false;
-const dragStart=()=>{
-     isDragging = true;
-     carousel.classList.add("dragging")
-}
-const dragging = (e)=>{
-    if(!isDragging) return;
-    carousel.scrollLeft = e.pageX;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidht = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+let isDragging = false, startX, startScrollLeft, timeoutId;
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidht);
 
-}
-carousel.addEventListener("mousemove",dragging);
-carousel.addEventListener("mousedown",dragStart)
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidht : firstCardWidht;
+    });
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+    if (!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+};
+
+const autoPlay = () => {
+    if (window.innerWidth < 800) return;
+    timeoutId = setTimeout(() => {
+        carousel.scrollLeft += firstCardWidht;
+        autoPlay(); // Call autoPlay() again for continuous scrolling
+    }, 2500);
+};
+
+autoPlay();
+
+const infiniteScroll = () => {
+    clearTimeout(timeoutId); // Clear existing timeout
+    if (!wrapper.matches(":hover")) autoPlay(); // Restart autoplay if not hovering
+};
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mousedown", dragStart);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
